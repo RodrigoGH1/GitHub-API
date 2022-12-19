490c1e9d9d29e222af62c91f5a3cee232acbdd87
RodrigoGH1 <rodney_github1@outlook.com>
2022-12-18 23:45:34 -0400

    feat: style search field

--- src/components/templates/SearchField/SearchField.tsx
+++ src/components/templates/SearchField/SearchField.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { Button, TextField, Typography } from '@mui/material'
+import styled from '@emotion/styled'
+import React, { useState } from 'react'
 
 /*
-The SearchField Component consists of multiple components.......
+The SearchField Component consists of multiple components managing the
+input fields necessary to query a GitHub account
 */
 
 export const SearchField = () => {
-    return <>Test</>
+    const [inputData, setInputData] = useState<string>('')
+    const [finalSearchInputVal, setFinalSearchInputVal] = useState<string>('')
+    const [renderChild, setRenderChild] = useState<boolean>(false)
+
+    const handleClick = (e: any) => {
+        setFinalSearchInputVal(inputData)
+        setRenderChild(true)
+    }
+
+    const format = /[ `!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]/
+    const isInputError = format.test(inputData)
+    return (
+        <div style={{ margin: '15px' }}>
+            <Typography>Search for a Github User</Typography>
+            <TextField
+                label="by Name"
+                data-testid="search-text-field"
+                variant="outlined"
+                onChange={(e: any) => setInputData(e.target.value)}
+                error={isInputError}
+                helperText="Cannot use special characters"
+            />
+            <StyledButton
+                variant="contained"
+                disabled={inputData === '' || isInputError}
+                data-testid="search-button"
+                onClick={(e: any) => handleClick(e)}
+            >
+                Search
+            </StyledButton>
+        </div>
+    )
 }
+
+export const StyledButton = styled(Button)`
+    margin-left: 15px;
+    margin-top: 18px;
+`
